4e1231e1d43ea8ea8b017045b080ec4d3665439f
Sumanta Ghosh <skg.onh08@gmail.com>
2018-02-18 12:55:06 +0530

    Data from Religare online

--- src/alphaVantage/input/ReadDataFromWeb/UpdateAllCompanyInfo.java
+++ src/alphaVantage/input/ReadDataFromWeb/UpdateAllCompanyInfo.java
@@ -1,7 +1,31 @@
 package alphaVantage.input.ReadDataFromWeb;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
 public class UpdateAllCompanyInfo {
 	
-	//testing 
+public static void main (String[] args){
+	
+		try {
+			Document doc = Jsoup.connect("http://www.religareonline.com/market/news").userAgent("Mozilla/17.0").get();
+			
+			Elements temp  = doc.select("div.s11.pT2.mB5");
+			
+			int i =0;
+			for (Element element : temp){
+				i++;
+				System.out.println(i+". "+element.getElementsByTag("a").last().text()); 
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+
+}
 
 }
